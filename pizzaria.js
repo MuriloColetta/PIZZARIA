d5817c2278d12aa916642c592cad51b31267f272
Murilo Della Coletta <mucoletta11@gmail.com>
2025-05-23 15:48:53 -0300

    Add files via upload

--- pizzaria.js
+++ pizzaria.js
@@ -1,4 +1,38 @@
+function exibirMensagem(texto, tipo) {
+  const mensagem = document.getElementById('mensagem')
+  mensagem.textContent = texto
+  mensagem.className = `mensagem ${tipo}`
+  mensagem.classList.remove('hedden')
+  setTimeout(() => {
+    mensagem.classList.add('hidden')
+  }, 3000)
+}
+
+function validarLogin() {
+  const usuario = document.getElementById('usuario').value
+  const senha = document.getElementById('senha').value
+  const usuarioCorreto = 'admin'
+  const senhaCorreta = '1234'
+  
+  if (usuario === usuarioCorreto && senha === senhaCorreta) {
+    exibirMensagem('Login realizado com sucesso!', 'sucesso')
+    setTimeout(() => {
+      window.location.href = 'pizzaria.html'
+    }, 1000)
+  } else {
+    exibirMensagem('Usuário ou senha incorretos.', 'Error')
+  }
+}
+
 let pizzaria = []
+let pizzaParaAlterar = null;
+
+function mostrarSecao(secao) {
+  document.getElementById("cadastro").classList.add("hidden");
+  document.getElementById("cardapio").classList.add("hidden");
+  document.getElementById("alterar").classList.add("hidden");
+  document.getElementById(secao).classList.remove("hidden");
+}
 
 function adicionarPizza() {
     const sabor = document.getElementById('sabor').value
