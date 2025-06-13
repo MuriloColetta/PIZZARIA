c71ac89819755c44338d28d347e2d332f83e3c6f
Murilo Della Coletta <mucoletta11@gmail.com>
2025-06-12 22:01:39 -0300

    Add files via upload

--- pizzaria.js
+++ pizzaria.js
@@ -20,7 +20,7 @@ function validarLogin() {
       window.location.href = 'pizzaria.html'
     }, 1000)
   } else {
-    exibirMensagem('Usuário ou senha incorretos.', 'Error')
+    exibirMensagem('Usuário ou senha incorretos.', 'error')
   }
 }
 
@@ -41,6 +41,12 @@ function adicionarPizza() {
     const ingredientes = document.getElementById('ingredientes').value
     const preco = parseFloat(document.getElementById('preco').value)
 
+    if (pizzaria.some(pizza => pizza.sabor.toLowerCase() === sabor.toLowerCase())) {
+    document.getElementById('erroCadastro').innerHTML = 'Este sabor já está cadastrado'
+    document.getElementById('sucessoCadastro').innerHTML = ''
+    return
+}
+
     if (sabor && ingredientes && preco) {
         pizzaria.push({sabor, ingredientes, preco})
         document.getElementById('sabor').value = ''
@@ -50,7 +56,7 @@ function adicionarPizza() {
         document.getElementById('sucessoCadastro').innerHTML = 'Pizza adicionada com sucesso!'
         document.getElementById('erroCadastro').innerHTML = ''
     } else {
-        document.getElementById('erroCadastro').innerHTML = ('Preencha todos os campos')
+        document.getElementById('erroCadastro').innerHTML = 'Preencha todos os campos'
         document.getElementById('sucessoCadastro').innerHTML = ''
     }
 }
@@ -86,9 +92,12 @@ function buscarPizzaParaAlterar() {
         document.getElementById('novo-sabor').value = pizzaParaAlterar.sabor
         document.getElementById('novo-ingredientes').value = pizzaParaAlterar.ingredientes
         document.getElementById('novo-preco').value = pizzaParaAlterar.preco
+        document.getElementById('erroBuscar').innerHTML = ''
     } else {
-        document.getElementById('erroBuscar').innerHTML = ('Pizza não encontrada.')
+        document.getElementById('erroBuscar').innerHTML = 'Pizza não encontrada'
         document.getElementById('form-alterar').classList.add('hidden')
+        document.getElementById('sucessoAlterar').innerHTML = ''
+        document.getElementById('erroAlterar').innerHTML = ''
     }
 }
 
@@ -105,11 +114,11 @@ function alterarPizza() {
 
             atualizarLista()
             document.getElementById('busca-alterar').value = ''
-            document.getElementById('sucessoAlterar').innerHTML = ('Pizza alterada com sucesso!')
+            document.getElementById('sucessoAlterar').innerHTML = 'Pizza alterada com sucesso!'
             document.getElementById('erroAlterar').innerHTML = ''
             document.getElementById('form-alterar').classList.add('hidden')
         } else {
-            document.getElementById('erroAlterar').innerHTML = ('Preencha todos os campos')
+            document.getElementById('erroAlterar').innerHTML = 'Preencha todos os campos'
             document.getElementById('sucessoAlterar').innerHTML = ''
         }
     }
@@ -120,30 +129,33 @@ let vendas = []
 function registrarVenda() {
     const sabor = document.getElementById('venda-sabor').value
     const cliente = document.getElementById('venda-cliente').value
+    const quantidade = document.getElementById('venda-quantidade').value
 
     const pizzaNoCardapio = pizzaria.find((pizza) => pizza.sabor.toLowerCase() === sabor.toLowerCase())
 
     if (pizzaNoCardapio) {
-        const preco = pizzaNoCardapio.preco
+        const precoTotal = pizzaNoCardapio.preco * quantidade
 
         if (cliente) {
             const listarVendas = document.getElementById('lista-vendas')
             const item = document.createElement('li')
-            item.textContent = `Sabor: ${sabor}, Preço: R$${preco.toFixed(2)}, Cliente: ${cliente}`
+            item.textContent = `Sabor: ${sabor}, Quantidade: ${quantidade}, Preço total: R$${precoTotal.toFixed(2)}, Cliente: ${cliente}`
             listarVendas.appendChild(item)
 
-            vendas.push({ sabor, preco, cliente })
+            vendas.push({ sabor, quantidade, precoTotal, cliente })
 
             document.getElementById('venda-sabor').value = ''
+            document.getElementById('venda-quantidade').value = ''
             document.getElementById('venda-cliente').value = ''
-            document.getElementById('sucessoVenda').innerHTML = 'Venda registrada.'
+            document.getElementById('sucessoVenda').innerHTML = 'Venda registrada com sucesso!'
             document.getElementById('erroVenda').innerHTML = ''
+            document.getElementById('erroRelatorio').innerHTML = ''
         } else {
-            document.getElementById('erroVenda').innerHTML = 'Preencha todos os campos.'
+            document.getElementById('erroVenda').innerHTML = 'Preencha todos os campos'
             document.getElementById('sucessoVenda').innerHTML = ''
         }
     } else {
-        document.getElementById('erroVenda').innerHTML = `A pizza de sabor "${sabor}" não está no cardápio.`
+        document.getElementById('erroVenda').innerHTML = `A pizza de sabor "${sabor}" não está no cardápio`
         document.getElementById('sucessoVenda').innerHTML = ''
     }
 }
@@ -166,17 +178,18 @@ function gerarRelatorioVendas() {
         linha.innerHTML = `
             <td>${venda.cliente}</td>
             <td>${venda.sabor}</td>
-            <td>R$${venda.preco.toFixed(2)}</td>
+            <td>${venda.quantidade}</td>
+            <td>R$${venda.precoTotal.toFixed(2)}</td>
             
         `
         tabelaRelatorio.appendChild(linha)
 
-        totalVendas += venda.preco;
+        totalVendas += venda.precoTotal;
     })
 
     const linhaTotal = document.createElement('tr')
     linhaTotal.innerHTML = `
-        <td colspan="2"><strong>Total de Vendas:</strong></td>
+        <td colspan="3"><strong>Total de Vendas:</strong></td>
         <td><strong>R$${totalVendas.toFixed(2)}</strong></td>
     `
     tabelaRelatorio.appendChild(linhaTotal)
